287eed044cde8148fbad09ba9d5fd3b290d1b816
Hrishikesh Kadam <hrkadam.92@gmail.com>
2018-11-21 11:52:54 +0530

    Add ZXing lib

--- app/src/main/java/com/codetoart/android/qrcodescannerandroid/MainActivity.kt
+++ app/src/main/java/com/codetoart/android/qrcodescannerandroid/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.codetoart.android.qrcodescannerandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_scan_qr_code.setOnClickListener {
+            IntentIntegrator(this).initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
